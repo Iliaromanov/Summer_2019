a4d12cf1ff9de44ab955102e6bcdf8e45356d13e
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2019-07-08 16:56:36 -0400

    Update creating_classes.py

--- Classes and Objects/creating_classes.py
+++ Classes and Objects/creating_classes.py
@@ -1,1 +1,20 @@
+class Snake(objetcs):  # '(objects)' is not necessary
+    def __init__(self, name):  
+        self.name = name
 
+    def talk(self):  # Method
+        print(f"Yo dawgs im {self.name}")
+
+    def change_name(self, new_name):  # Method
+        self.name = new_name
+
+
+bartholamule = Snake("Barhtolamule")
+
+opa = Snake('Opa')
+
+opa.change_name('Dundas')
+
+if __name__ == "__main__":
+    bartholamule.talk()
+    opa.talk()
