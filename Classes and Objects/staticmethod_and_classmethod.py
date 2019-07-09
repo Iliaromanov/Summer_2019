aed44add5e71bb3480a33ae3a89cde812c831ce5
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2019-07-09 14:45:42 -0400

    Create staticmethod_and_classmethod.py

---
+++ Classes and Objects/staticmethod_and_classmethod.py
@@ -0,0 +1,30 @@
+# Basically Useless
+
+# Class method is to access class variables without having any objects
+
+# static method is to make functions not related to self within a class
+
+class Dog:
+
+    dogs = []
+
+    def __init__(self, name):
+        self.name = name
+        self.dogs.append(self)
+
+    @classmethod
+    def num_dogs(cls):
+        return len(cls.dogs)
+
+    @staticmethod
+    def bark(n):
+        for _ in range(n):
+            print("Bark!")
+
+
+bob = Dog("Bob")
+bret = Dog("Bret")
+
+print(Dog.num_dogs())  # Without class method would have to say bob.num_dogs() or bret.num_dogs()
+
+Dog.bark(4) 
